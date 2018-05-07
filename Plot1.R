1d76a675046080632f4394402f5d2a831444f974
NCABIOCH <36126273+NCABIOCH@users.noreply.github.com>
2018-05-07 09:50:42 +0200

    Update for generalization
    Remove work directory setting
    Update png file name to capitalize first letter

--- Plot1.R
+++ Plot1.R
@@ -1,7 +1,4 @@
 ## Getting full dataset
-setwd("~/R/Exploratory Graphs/Week1")
-
-## Getting full dataset
 data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
@@ -17,10 +14,9 @@ datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
 
 ## Plot 1
-## png("plot1.png", width = 480, height = 480) 
 hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
 ## Saving to file
- dev.copy(png, file="cp_plot1.png", height=480, width=480)
+dev.copy(png, file="Plot1.png", height=480, width=480)
 
 dev.off()
